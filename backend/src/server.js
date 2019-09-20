0ebeba7fcc4b5004809eff041c714415895f3f05
Edson Rodrigues <edsonrodsilva@gmail.com>
2019-09-20 10:50:46 -0300

    sinc...

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
+
 const server = express()
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-cfj16.mongodb.net/timdev?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+})
+
 server.use(express.json())
+
 server.use(routes)
+
 server.listen(3333)
 
 
