30e65c371104b042ad0af0dadca50d6b25caf6aa
Edson Rodrigues <edsonrodsilva@gmail.com>
2019-10-29 16:23:36 -0300

    routes ajustes...

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,4 +16,8 @@ routes.post('/devs/:devId/likes', LikeController.store)
 //dislikes
 routes.post('/devs/:devId/dislikes', DislikeController.store)
 
+
+//dislikes
+//routes.post('/devs/:devId/dislikes', DislikeController.store)
+
 module.exports = routes
